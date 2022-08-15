5258b0c5f67681b0a7a7218db04d4a701ac05929
aidar-yessembayev <98548293+aidar-yessembayev@users.noreply.github.com>
2022-08-15 13:06:39 +0600

    Update Square.java
    init

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,6 +2,28 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
+        if (sideLength == 0) {
+            System.out.println("");
+        } else {
+            int start = 1;
+            int finish = sideLength;
 
+            for (int i = 1; i <= sideLength; i++) {
+                if (i == start || i == finish) {
+                    for (int j = 1; j <= sideLength; j++) {
+                        System.out.print("8");
+                    }
+                } else {
+                    for (int j = 1; j <= sideLength; j++) {
+                        if (j == 1 || j == sideLength) {
+                            System.out.print("8");
+                        } else {
+                            System.out.print(" ");
+                        }
+                    }
+                }
+                System.out.println();
+            }
+        }
     }
 }
